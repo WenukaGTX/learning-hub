627e4d4dedcb6c7f00ce30b11590f76fefb3b51f
Suresh Godakanda <wenukagtx@gmail.com>
2024-04-25 14:34:02 +0530

    successStories

---
+++ src/components/SuccessStories/index.js
@@ -0,0 +1,48 @@
+import Card from '../../components/Card';
+
+const successStoryItems = [
+  {
+    "image": "https://placehold.co/120",
+    "title": "Title goes here",
+    "description": "Subtitle goes here"
+  },
+  {
+    "image": "https://placehold.co/120",
+    "title": "Title goes here",
+    "description": "Subtitle goes here"
+  },
+  {
+    "image": "https://placehold.co/120",
+    "title": "Title goes here",
+    "description": "Subtitle goes here"
+  },
+  {
+    "image": "https://placehold.co/120",
+    "title": "Title goes here",
+    "description": "Subtitle goes here"
+  }
+];
+
+function successStories(props) {
+  return (
+    <div className='pt-4'>
+      <div className="container">
+        <h2 className='text-center mb-3'>High scorers</h2>
+        <div className='row'>
+          {successStoryItems.map((item) => (
+            <div className='col-4' key={item.id}>
+              <Card
+                image={item.image}
+                cardImageSize={240}
+                title={item.title}
+                description={item.description}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default successStories;
