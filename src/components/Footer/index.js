bc977406aac878cd50d625a296ed6208fb8a0c4f
Suresh Godakanda <wenukagtx@gmail.com>
2024-04-26 18:11:52 +0530

    footer refreshed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,38 +4,25 @@ import logo from '../../images/qb-logo.svg';
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <div className='wrapper'>
-          <div className='footer-bottom-container'>
-
-            <div className="footer-col top">
-              <Link to='/' ><img src={logo} alt="Logo" /></Link>
-            </div>
-
-            <div className="footer-col bottom">
-                <div className='footer-list-container left'>
-                    <p>© 2022 Intuit Inc. All rights reserved.</p>
-                    <p>Intuit, QuickBooks, QB, TurboTax, ProConnect, and Mint are registered trademarks of Intuit Inc. Terms and conditions, features, support, pricing, and service options subject to change without notice.<br></br><br></br>By accessing and using this page you agree to the Terms and Conditions.</p>
-                    <ul className='footer-list'>
-                        <li><Link to='/' >Terms of Service</Link></li>
-                        
-                        <li><Link to='/' >Contact</Link></li>
-                    </ul>
-                </div>
-                <div className='footer-list-container right'>
-                    <ul className='footer-list footer-bottom-links'>
-                        <li><Link to='/' >Legal</Link></li>
-                        
-                        <li><Link to='/' >Privacy</Link></li>
-
-                        <li><Link to='/' >Security</Link></li>
-                    </ul>
-                </div>
-                
-            </div>
-          </div>
+    <footer className="footer">
+      <div className='container'>
+        <Link to='/' className='footer-logo'><img src={logo} alt="Logo" /></Link>
+        <div className='footer-right'>
+          <p className='footer-text'>© 2022 Intuit Inc. All rights reserved</p>
+          <nav>
+            <Link to='/' className='white'>Terms of Service</Link>
+            <p class="spacer white">|</p>
+            <Link to='/' className='white'>Contact</Link>
+            <p class="spacer white">|</p>
+            <Link to='/' className='white'>Legal</Link>
+            <p class="spacer white">|</p>
+            <Link to='/' className='white'>Privacy</Link>
+            <p class="spacer white">|</p>
+            <Link to='/' className='white'>Security</Link>
+          </nav>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
