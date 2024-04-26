fc274589a98cb50f6edbcd0e7bb32a21ba087cdb
Suresh Godakanda <wenukagtx@gmail.com>
2024-04-26 17:13:32 +0530

    according refreshed

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -9,39 +9,30 @@ function FrequentQuestions() {
 
     useEffect(() => {
         fetch('https://662898c654afcabd0736410c.mockapi.io/intuit/training/reactjs/FrequentlyAskedQuestions')
-          .then(response => response.json())
-          .then(data => setFaqset(data))
-          .catch(error => console.error('Error fetching data:', error));    
-      }, []);
-
+            .then(response => response.json())
+            .then(data => setFaqset(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
     const renderFAQs = () => {
         return faqset && faqset.map((item) => {
             return (
-                <Accordion  
+                <Accordion
                     key={item.id}
-                    question={item.question} 
+                    question={item.question}
                     answer={item.answer}
-                 />
+                />
             );
         })
     }
 
-
     return (
-        <div className='faq-section'>
-            <div className='wrapper'>
-                <section className="section type0">
-                    <div className='content-area'>
-                        <h2>Frequently asked questions</h2>
-
-                         {renderFAQs()}
-                         
-                    </div>
-                </section>
+        <section className='section-padding bt-ash'>
+            <div className='container'>
+                <h2 className='mb-4 text-center'>Frequently asked questions</h2>
+                {renderFAQs()}
             </div>
-        </div>
-        
+        </section>
     )
 }
 
