56456e16c52ed38607252092aca0d68c1643f0b3
Suresh Godakanda <wenukagtx@gmail.com>
2024-04-29 12:04:56 +0530

    responsive-fixes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,8 +7,8 @@ function Navbar() {
     <header className="navbar">
       <div className='container'>
         <Link to='/' className='flex'><img src={logo} alt="Logo" /></Link>
-        <nav className="navbar-right">
-          <Link to='/sign-up' className='button button-primary'>Sign up for FREE</Link>
+        <nav className="nav-links nav-links-right">
+          <Link to='/sign-up' className='button button-primary'>Sign up<span className='hide-xs'> for FREE</span></Link>
         </nav>
       </div>
     </header>
