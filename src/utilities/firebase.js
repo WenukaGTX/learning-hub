f298dea3493983c4c2951184b68fcabd4c725024
Suresh Godakanda <wenukagtx@gmail.com>
2024-05-10 10:48:23 +0530

    ref

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBMNpJdJlInmiJgL7uwfG7LRERL9l_1T_0",
     authDomain: "learning-hub-v1.firebaseapp.com",
@@ -17,9 +11,10 @@ const firebaseConfig = {
     measurementId: "G-SYMES05KF0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-// const analytics = getAnalytics(app);
 
 export { app, auth };
+
+// Sample repo:
+// https://github.com/Nitij/react-firebase-auth-boilerplate/tree/main
